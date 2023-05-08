8d61765ae621ea4d7d2acd51145f9cca5093ba81
JuanAbad <82122031+JuanAbad@users.noreply.github.com>
2023-05-08 17:25:14 +0200

    Commit de MyClinicControlº
    Cambios hechos: Añadido sistema de citas, sistema de visualización de citas médicas por orden, sistema de añadir citas médicas, sistema de detalle de paciente

---
+++ MyClinicControl/Controller/ControllerCitas.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once("../Model/Cita.php");
+require_once("../Model/model.php");
+
+class ControllerCitas{
+    /*Método que llama al model para obtener todos los datos de las citas
+    por el mes y por el dia */
+    public function getCitasByData($dia,$mes,$annio){
+        $modelo = new Model();
+        echo $annio;
+        $personas = $modelo->getCitasByDate($dia,$mes,$annio);
+        return $personas;
+    }
+}
+/*Ya que los campos de añadir citas son required, si existe el campo titulo,
+Me recogerá los datos de añadir cita para añadirlos en la bbdd
+PD: Los datos del nombre del paciente, lo pongo en vacio, ya que esos he optado
+por cogerlos en la model  */
+if(isset($_POST['titulo'])){
+    $dia = $_POST['dia'];
+    $mes = $_POST['mes'];
+    $annio = $_POST['annio'];
+    $titulo = $_POST['titulo'];
+    $dnipaciente = $_POST['paciente'];
+    $detalles = $_POST['detalles'];
+    $hora_inicio = $_POST['hora_inicio'];
+    $hora_fin = $_POST['hora_fin'];
+    
+    $cita = new Cita($titulo,$dnipaciente,"",$detalles,$hora_inicio,$hora_fin,$dia,$mes,$annio);
+
+        $modelo = new Model();
+        $modelo->insertCitas($cita);
+    header("location: ../View/calendario.php");
+}
+/*Método para poder loguearme el doctor, y comprobarme que el email y la contraseña
+son correctos, en caso contrario, me redirigirá a login.php */
+if(isset($_POST['emailogindoctor'])){
+    $emailogindoctor = $_POST['emailogindoctor'];
+    $contrasenalogindoctor = $_POST['contrasenalogindoctor'];
+    echo $emailogindoctor;
+    echo $contrasenalogindoctor;
+    $modelo = new Model();
+    $doctorLogueado = $modelo->loginDoctor($emailogindoctor,$contrasenalogindoctor);
+    if($doctorLogueado){
+        header("location: ../view/contactos.php");
+        session_start();
+        $_SESSION['nombreDoctor'] = $doctorLogueado->getNombre();
+        $_SESSION['dniDoctor'] = $doctorLogueado->getDni();
+        echo $_SESSION['nombreDoctor'];
+        setcookie("dniDoctor",$doctorLogueado->getDni(),time()+(86400 * 30),"/");
+    }
+    else{
+        header("location: ../View/login.php");
+    } 
+}
+/*Método que me eliminará de la sesion el dato de la sesión, redirigiendome
+a index.php*/
+if(isset($_POST['cerrarSesion'])){
+    unset($_SESSION['nombreDoctor']);
+    header("location: ../View/index.php");
+}
+?>
---
+++ MyClinicControl/Controller/ControllerPacientes.php
@@ -0,0 +1,115 @@
+<?php
+require_once("../Model/model.php");
+require_once("../Model/Paciente.php");
+class ControllerPacientes
+{
+  private $pacientes;
+  /*Función incial que llama a la model para mostrar TODOS los pacientes del usuario
+    que esté usando la aplicación*/
+  function showDataTable($table, $condition)
+  {
+
+    $datos = new Model();
+    $this->pacientes = $datos->selectData($table, 1);
+
+    return $this->pacientes;
+  }
+  /*Función que llama a la model para guardar los pacientes*/
+  function savePaciente($table, $paciente)
+  {
+    $datos = new Model();
+    $this->pacientes = $datos->insertPacientes($table, $paciente);
+  }
+  function updatePaciente($paciente){
+    $datos = new Model();
+    $this->pacientes = $datos->modificarPaciente($paciente);
+    return $this->pacientes;
+  }
+  /*Función que llama a la model para poder sacar los pacientes
+    por dni,nombre o apellidos*/
+  function GetPacientesByData($identificador)
+  {
+    
+    $identificador = trim($identificador);
+    $identificadorConComillas = '"'.$identificador.'"';
+    
+
+    $modelo = new Model();
+
+    $this->pacientes = $modelo->selectPacientesByDni($identificador);
+
+    return $this->pacientes;
+  }
+  function getJsonFromPacienteByData($identificador)
+  {
+    $modelo = new Model();
+
+    $pacientes = $modelo->selectPacientesByDni($identificador);
+    
+    header('Content-Type: application/json');
+    
+  }
+  /*Función que recoge el orden elegido en la view para enviarselo
+    a la model*/
+  function GetPacientesByOrder($orden)
+  {
+    $modelo = new Model();
+    $personasOrdenadas = $modelo->selectData("pacientes", $orden);
+    return $personasOrdenadas;
+  }
+}
+
+
+if ([$_SERVER['REQUEST_METHOD'] == 'POST']) {
+  /*Función que llama a la clase del controller para coger los datos de un solo
+  paciente, ya que se identifica por su dni*/
+  if (isset($_GET['dni'])) {
+    $identificador = $_GET['dni'];
+    $controller = new ControllerPacientes();
+    $pacientesFiltrados = $controller->GetPacientesByData($identificador);
+    echo json_encode($pacientesFiltrados);
+  }
+  if(isset($_GET['dnimodificar'])){
+    $dni = $_POST['dni'];
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $edad = $_POST['edad'];
+    $sexo = $_POST['sexo'];
+    $telefono = $_POST['telefono'];
+    $patologia = $_POST['patologias'];
+
+    $paciente = new Paciente($dni, $nombre, $apellidos, $edad, $sexo, $telefono, $patologia);
+
+    $controller = new ControllerPacientes();
+    $pacientes = $controller->updatePaciente($paciente);
+    header("location: ../View/contactos.php");
+  }
+  /* Función que borra a un paciente identificandolo por su dni llamando a la 
+  clase del controller que se encarga de llamar a la model para eliminarlo */
+  if (isset($_POST['dnipaciente'])) {
+    $dnipaciente = $_POST['dnipaciente'];
+    echo $dnipaciente;
+    $modelo = new Model();
+    $modelo->borrarPaciente("pacientes", $dnipaciente);
+    header("location: ../View/contactos.php");
+  }
+  /*Función que recoge el texto de busqueda de busqueda de paciente
+    Y llama a la model para buscar ese contacto en específico*/
+  if (isset($_POST['nombre']) && isset($_POST['apellidos'])&& !isset($_GET['dnimodificar'])) {
+    /*Como esta comprobación esta fuera de la clase, crearemos atributos propios
+    dni,nombre...*/
+    $dni = $_POST['dni'];
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $edad = $_POST['edad'];
+    $sexo = $_POST['sexo'];
+    $telefono = $_POST['telefono'];
+    $patologia = $_POST['patologias'];
+
+    $paciente = new Paciente($dni, $nombre, $apellidos, $edad, $sexo, $telefono, $patologia);
+    $modelo = new Model();
+    $modelo->insertPacientes("pacientes", $paciente);
+
+    header("location: ../View/contactos.php");
+  }
+}
