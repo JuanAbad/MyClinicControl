3d674982fa35175ffe35a7adf123996c708615f6
JuanAbad <82122031+JuanAbad@users.noreply.github.com>
2023-05-17 12:00:37 +0200

    Add files via upload

---
+++ MyClinicControl/Controller/ControllerCitas.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once("../Model/Cita.php");
+require_once("../Model/model.php");
+
+class ControllerCitas{
+    /*Método que llama al model para obtener todos los datos de las citas
+    por el mes y por el dia */
+    public function getCitasByData($dia,$mes,$annio){
+        $modelo = new Model();
+        
+        $personas = $modelo->getCitasByDate($dia,$mes,$annio);
+        return $personas;
+    }
+    public function getCitasByDni($dni){
+        $modelo = new Model();
+        
+        $personas = $modelo->getCitasByDni($dni);
+        return $personas;
+    }
+    
+    public function getDayMesAndAnnio(){
+        $modelo = new Model();
+        $fechaCitas = $modelo->selectDayMonthAndYear();
+        return $fechaCitas;
+    }
+
+    public function getDayMesAndAnnioByDni($dni){
+        $modelo = new Model();
+        $fechaCitas = $modelo->selectDayMonthAndYearByDni($dni);
+        return $fechaCitas;
+    }
+}
+/*Ya que los campos de añadir citas son required, si existe el campo titulo,
+Me recogerá los datos de añadir cita para añadirlos en la bbdd
+PD: Los datos del nombre del paciente, lo pongo en vacio, ya que esos he optado
+por cogerlos en la model  */
+if(isset($_POST['titulo'])){
+    $dia = $_POST['dia'];
+    $mes = $_POST['mes'];
+    $annio = $_POST['annio'];
+    $titulo = $_POST['titulo'];
+    $dnipaciente = $_POST['paciente'];
+    $detalles = $_POST['detalles'];
+    $hora_inicio = $_POST['hora_inicio'];
+    $hora_fin = $_POST['hora_fin'];
+    
+    $cita = new Cita($titulo,$dnipaciente,"",$detalles,$hora_inicio,$hora_fin,$dia,$mes,$annio);
+
+        $modelo = new Model();
+        $modelo->insertCitas($cita);
+    header("location: ../View/calendario.php");
+}
+?>
---
+++ MyClinicControl/Model/NombreDoctor.php
@@ -0,0 +1,20 @@
+<?php
+
+class NombreDoctor{
+    public $nombre;
+    function __construct($nombre)
+    {
+        $this->nombre = $nombre; 
+    }
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+    public function getNombre() {
+        return $this->nombre;
+    }
+    public function __toString() {
+        return $this->nombre;
+    }
+}
+
+?>
